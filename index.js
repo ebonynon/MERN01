d21362ae905b42df2b984d503ebfb03b7b8ab612
ebonynon <sithumsandeepap@gmail.com>
2020-06-13 04:28:28 +0530

    app.j:5

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get('/', (req, res) => res.send('Hello world!'));
 // use Routes
 //app.use('/api/books', books);
 
-const port = 5000 //process.env.PORT || 8082;
+const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
