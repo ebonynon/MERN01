4655155918ced2e9a3bdb1ba2c09f25633d155b9
ebonynon <sithumsandeepap@gmail.com>
2020-06-13 04:37:22 +0530

    app.j:8

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const books = require('./routes/api/books');
 const app = express();
 
 // Connect Database
-//connectDB();
+//connectDB(); xx
 
 // cors
 //app.use(cors({ origin: true, credentials: true }));
