5d3708f9e7e3aa95060f59a2d54b26068c5c9abe
ebonynon <sithumsandeepap@gmail.com>
2020-06-13 05:37:43 +0530

    db.j:5 n many

--- config/db.js
+++ config/db.js
@@ -1,19 +1,27 @@
- const mongoose = require('mongoose');
- const config = require('config');
-//const db = config.get('mongoURI'); xxx
+const mongoose = require('mongoose');
+//const config = require('config');
+//const db = config.get('mongoURI');
+const mURI = process.env.MONGODB_URI;
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const db = mURI;
 
 const connectDB = async () => {
-  // try {
-  //   await mongoose.connect(
-  //     db,
-  //     { useNewUrlParser: true }
-  //   );
+  try {
+    await mongoose.connect(
+      db,
+      { useNewUrlParser: true }
+    );
 
     console.log('MongoDB is Connected...');
-  // } catch (err) {
-  //   console.error(err.message);
-  //   process.exit(1);
-  // }
+    console.log(`MongoDB :: ${mURI}`);
+
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
