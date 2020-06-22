cb2343f7c5f07247b3c9b385015ba03488f65f5a
ebonynon <sithumsandeepap@gmail.com>
2020-06-22 15:04:22 +0530

    db old

--- config/db.js
+++ config/db.js
@@ -1,27 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-//const config = require('config');
-//const db = config.get('mongoURI');
 
 dotenv.config();
 
 const db = process.env.MONGODB_URI;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      db,
-      { useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: true }
-    );
+const mongoose = require('mongoose')
 
-    console.log('MongoDB is Connected...');
-
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+})
