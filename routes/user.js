62331837da56c8ca33e2f3ea88a62996ba6242b8
ebonynon <sithumsandeepap@gmail.com>
2020-06-29 21:47:49 +0530

    :?

--- routes/user.js
+++ routes/user.js
@@ -21,27 +21,27 @@ const router = express.Router();
 //    }
 //});
 
-router.post("/", async (req, res) => {	// /users ---> /
-  // Create a new user
-  User.new()
-    // .then(
-    //   const user = new User(req.body);
-    //   await user.save();
-    //   const token = await user.generateAuthToken();
-    //   res.status(201).send({ user, token });
-    // )
-    // .catch((error) {
-    //   res.status(400).send(error);
-    // ) 
-  try {
-    const user = new User(req.body);
-    await user.save();
-    const token = await user.generateAuthToken();
-    res.status(201).send({ user, token });
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+// router.post("/", async (req, res) => {	// /users ---> /
+//   // Create a new user
+//   User.new()
+//     // .then(
+//     //   const user = new User(req.body);
+//     //   await user.save();
+//     //   const token = await user.generateAuthToken();
+//     //   res.status(201).send({ user, token });
+//     // )
+//     // .catch((error) {
+//     //   res.status(400).send(error);
+//     // ) 
+//   try {
+//     const user = new User(req.body);
+//     await user.save();
+//     const token = await user.generateAuthToken();
+//     res.status(201).send({ user, token });
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// });
 
 router.post("/", async (req, res) => {	// /users ---> /
   // Create a new user
